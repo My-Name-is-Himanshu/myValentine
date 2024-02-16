cde03d2d248f85f9d8212bbe7ba0f6260dda8241
My-Name-is-Himanshu <160240025+My-Name-is-Himanshu@users.noreply.github.com>
2024-02-17 00:22:48 +0530

    adding file

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who do you love?",
+        answer: "C. Me",
+        options: [
+            "A. Your friends",
+            "B. Your family",
+            "C. Me",
+            "D. Food"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What do you want from me?",
+        answer: "A. Love",
+        options: [
+            "A. Love",
+            "B. Kids",
+            "C. Marriage",
+            "D. Nothing"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What makes you angry?",
+        answer: "A. Bihari",
+        options: [
+            "A. Bihari",
+            "B. Me",
+            "C. Your friends",
+            "D. Family"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What do i need to do better?",
+        answer: "D. Understand You",
+        options: [
+            "A. Love you more",
+            "B. Beat you more!!",
+            "C. Fk you more",
+            "D. Understand You"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What you want to eat next time?",
+        answer: "D. Gajar ka halwa",
+        options: [
+            "A. Moburg",
+            "B. Burger King se burger",
+            "C. Pizza",
+            "D. Gajar ka halwa"
+        ]
+    }
+];
